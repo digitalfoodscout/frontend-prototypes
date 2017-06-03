0ce5fc4cde6f6e59e4317683163ed880406ff011
Dustin Chabrowski <dustin.duske@gmail.com>
2017-06-03 18:57:12 +0200

    (feat) add back-to-login-page button on register page

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
-import Header from '../components/Header'
-import RegisterScreen from '../components/RegisterScreen'
-import * as actions from '../actions'
+import React from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import Divider from 'material-ui/Divider';
+import * as actions from '../actions';
 import {NAV_REGISTER} from '../constants/NavigationStates';
+import Header from '../components/Header';
+import RegisterScreen from '../components/RegisterScreen';
+import {buttonStyle}  from '../components/FormFields';
+
 
 const Register = ({nav, title, user, actions}) => (
   <div>
     <Header nav={nav} title={title}/>
     <RegisterScreen onSubmit={actions.submitRegister} user={user}/>
+    <Divider/>
+    <Link to="/login">
+      <RaisedButton label="Zurück zum Login" style={buttonStyle}/>
+    </Link>
   </div>
 );
 
