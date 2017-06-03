cf538613ea5e6eea92527735f569a5324f4727f5
Dustin <dustin.duske@gmail.com>
2017-06-03 16:44:29 +0200

    (feat) implement header bar (#5)
    * (feat) include basic components for generic header
    
    * (feat) create DFS logo as an SVG component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import LogoImg from '../resources/img/logo_round.png';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import LogoIcon from '../resources/svg/Logo';
+
+const Logged = (props) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Settings"/>
+    <MenuItem primaryText="Logout"/>
+  </IconMenu>
+);
 
 // rewritten as stateless functional component, see https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html#stateless-functional-components
 export default function Header(props) {
   const title = props.title ? props.title : "DigitalFoodScout";
-
   return (
-    <header className="header">
-      <h1>{title}</h1>
-      <h2>{props.nav}</h2>
-      <div style={{display: 'flex', justifyContent: 'center', padding: "8px"}}>
-        <img src={LogoImg} style={{width: "50%", height: "50%"}} alt="Digital FoodScout Logo"/>
-      </div>
-    </header>
+    <AppBar
+      title={<span>{title}</span>}
+      iconElementLeft={<IconButton><LogoIcon /></IconButton>}
+      iconElementRight={<Logged/>}
+    />
   )
 }
 
 Header.propTypes = {
-  nav: PropTypes.string.isRequired,
   title: PropTypes.string
 };
